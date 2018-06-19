cb0863e9b23f0486dd9b82b9cf79a88a93eac7bb
steven <steven.hampton415@gmail.com>
2018-06-19 12:22:35 -0500

    Made fragment files, and made posts getters and setters

--- src/main/java/com/codeup/blog/MyFirstController.java
+++ src/main/java/com/codeup/blog/MyFirstController.java
@@ -9,37 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MyFirstController {
 
-    @GetMapping("/say-hello")
-    public @ResponseBody String myFirstSpringView() {
-        return "Hello from Spring!";
-    }
-
     @GetMapping("/hello/{name}")
     public String sayHello(@PathVariable String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
     }
 
-    @GetMapping("/say-hello/{name}")
-    public @ResponseBody String sayHello(@PathVariable String name){
-        return String.format("Hello, %s!", name);
-    }
-
-    @GetMapping("/flights/from/{from}/to/{to}")
-    public @ResponseBody String getFlights(
-            @PathVariable String from,
-            @PathVariable String to
-    ) {
-        return String.format("Finding flights from %s to %s...", from, to);
-    }
-
-    @GetMapping("/say-goodbye")
-    public @ResponseBody String mySecondSpringView() {
-        return "Goodbye World!";
-    }
-
-    @GetMapping("increment/{n}")
-    public @ResponseBody String increment(@PathVariable int n){
-        return String.format("%d + 1 = %d", n, n + 1);
+    @GetMapping("/hello/")
+    public String sayHello() {
+        return "hello";
     }
 }
--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,37 +1,51 @@
 package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
 
-    @GetMapping("/posts")
-    public @ResponseBody String postsIndex(){
-        return "posts index page";
+    @RequestMapping(path="/posts", method= RequestMethod.GET)
+    public String postsIndex(Model model) {
+        List<Post> posts = new ArrayList<>();
+        Post post1 = new Post("Test Post #1","Random body stuff #1");
+        Post post2 = new Post("Test Post #2","Random body stuff #2");
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public @ResponseBody String showDetails(@PathVariable long id){
-        return "view an individual post #" + id;
+    public String showDetails(@PathVariable long id, Model model) {
+        model.addAttribute("id",id);
+        Post post1 = new Post("Test Post","Random body stuff");
+        model.addAttribute("postTitle", post1.getTitle());
+        model.addAttribute("postBody", post1.getBody());
+        return "posts/show";
     }
 
     @GetMapping("/posts/{id}/edit")
-    public @ResponseBody String edit(@PathVariable long id){
+    public @ResponseBody
+    String edit(@PathVariable long id) {
         return "View the from for editing post #" + id;
     }
 
     @GetMapping("/posts/create")
-    public @ResponseBody String create(){
+    public @ResponseBody
+    String create() {
         return "View the form for creating a post";
     }
 
     @PostMapping("/posts/create")
-    public @ResponseBody String savePost(){
+    public @ResponseBody
+    String savePost() {
         return "Saving to the databases";
     }
 }
